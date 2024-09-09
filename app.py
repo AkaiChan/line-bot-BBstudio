a4063ca653df2b966e0846ed3195e09d12abd380
Akai Chan <bruce20219@gmail.com>
2024-09-10 00:04:21 +0800

    Add flexmessage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import psycopg2
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
 import os
 
 app = Flask(__name__)
