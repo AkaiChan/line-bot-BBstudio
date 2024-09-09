f596e29253917fb9c0bac9388674533677ec60f4
Akai Chan <bruce20219@gmail.com>
2024-09-09 20:03:09 +0800

    add weather information

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -8,7 +10,18 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+OPENWEATHER_API_KEY = os.environ['OPENWEATHER_API_KEY']  # 請確保設置這個環境變數
 
+def get_weather(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=metric&lang=zh_tw"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        weather_description = data['weather'][0]['description']
+        temperature = data['main']['temp']
+        return f"{city}的天氣：{weather_description}，溫度：{temperature}°C"
+    else:
+        return "抱歉，無法獲取天氣資訊。"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -25,6 +38,9 @@ def callback():
 def handle_message(event):
     if event.message.text == "哈囉":
         reply_text = "你好嗎？"
+    elif "天氣" in event.message.text:
+        city = "台北"  # 預設城市，您可以根據需要修改
+        reply_text = get_weather(city)
     else:
         reply_text = event.message.text
     
