0aac44230ba60e0f37ee1de87060d5b94b3f2f1f
Akai Chan <bruce20219@gmail.com>
2024-09-12 20:32:47 +0800

    Add stock API

--- stock_api.py
+++ stock_api.py
@@ -19,17 +19,17 @@ class TWStockAPI:
             stock_name = TWStockAPI.get_stock_name(stock_code)
             
             return {
-                "股票代碼": stock_code,
+                "股票代碼": stock_code,  # 確保這個鍵存在
                 "股票名稱": stock_name if stock_name else "未知",
-                "日期": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                "成交股數": latest_data["TradeVolume"],
-                "成交金額": latest_data["TradeValue"],
-                "開盤價": latest_data["OpeningPrice"],
-                "最高價": latest_data["HighestPrice"],
-                "最低價": latest_data["LowestPrice"],
-                "收盤價": latest_data["ClosingPrice"],
-                "漲跌": latest_data["Change"],
-                "成交筆數": latest_data["Transaction"]
+                "日期": latest_data.get("Date", datetime.now().strftime("%Y-%m-%d")),
+                "成交股數": latest_data.get("TradeVolume", "N/A"),
+                "成交金額": latest_data.get("TradeValue", "N/A"),
+                "開盤價": latest_data.get("OpeningPrice", "N/A"),
+                "最高價": latest_data.get("HighestPrice", "N/A"),
+                "最低價": latest_data.get("LowestPrice", "N/A"),
+                "收盤價": latest_data.get("ClosingPrice", "N/A"),
+                "漲跌價差": latest_data.get("Change", "N/A"),
+                "成交筆數": latest_data.get("Transaction", "N/A")
             }
         except requests.RequestException as e:
             return f"獲取股票 {stock_code} 信息時發生網絡錯誤：{str(e)}"
