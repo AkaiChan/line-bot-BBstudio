39ccb256b08475aa6df3de4d183c6202a429254c
Akai Chan <bruce20219@gmail.com>
2024-09-16 00:16:16 +0800

    Add stock happy 5 line

--- line_member_system.py
+++ line_member_system.py
@@ -11,26 +11,26 @@ class LineMemberSystem:
                 cur.execute("SELECT * FROM line_members WHERE line_user_id = %s", (line_user_id,))
                 return cur.fetchone()
 
-    def register_member(self, line_user_id, display_name):
+    def register_member(self, line_user_id, display_name, current_time):
         with self.get_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute('''
-                INSERT INTO line_members (line_user_id, display_name)
-                VALUES (%s, %s)
+                INSERT INTO line_members (line_user_id, display_name, created_at, last_interaction)
+                VALUES (%s, %s, %s, %s)
                 ON CONFLICT (line_user_id) DO UPDATE
                 SET display_name = EXCLUDED.display_name,
-                    last_interaction = CURRENT_TIMESTAMP
-                ''', (line_user_id, display_name))
+                    last_interaction = EXCLUDED.last_interaction
+                ''', (line_user_id, display_name, current_time, current_time))
             conn.commit()
 
-    def update_last_interaction(self, line_user_id):
+    def update_last_interaction(self, line_user_id, current_time):
         with self.get_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute('''
                 UPDATE line_members
-                SET last_interaction = CURRENT_TIMESTAMP
+                SET last_interaction = %s
                 WHERE line_user_id = %s
-                ''', (line_user_id,))
+                ''', (current_time, line_user_id))
             conn.commit()
 
     def add_points(self, line_user_id, points):
